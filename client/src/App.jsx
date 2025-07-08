cb672d11745b0b1bd9567b95510f75ff35d4f343
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-07-08 14:38:22 +0200

    Implement dashboard layout with navigation and add job form featuring title, description (Quill editor), category, location, level, salary fields

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,31 @@ import Applications from "./pages/Applications";
 import { AppContext, AppContextProvider } from "./context/AppContext";
 import LoginRecruiter from "./components/LoginRecruiter";
 import { useContext } from "react";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplications from "./pages/ViewApplications";
+import 'quill/dist/quill.snow.css'
 const App = () => {
-const {showRecruiterLogin} = useContext(AppContext)
+  const { showRecruiterLogin } = useContext(AppContext);
 
   return (
     <div>
-    <BrowserRouter>
-      <>
-    { showRecruiterLogin && <LoginRecruiter/>}
+      <BrowserRouter>
+        <>
+          {showRecruiterLogin && <LoginRecruiter />}
           <Routes>
-            <Route path="/" element={ <Home/>}/>
-            <Route path="/apply-job/:id" element={<ApplyJob/>} />
-            <Route path = "/applications" element={<Applications/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/apply-job/:id" element={<ApplyJob />} />
+            <Route path="/applications" element={<Applications />} />
+            <Route path="/dashboard" element={<Dashboard />}>
+              <Route path="add-job" element={<AddJob />} />
+              <Route path="manage-jobs" element={<ManageJobs />} />
+              <Route path="view-applications" element={<ViewApplications />} />
+            </Route>
           </Routes>
-      </>
-    </BrowserRouter>
+        </>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ client/src/pages/AddJob.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useRef, useState } from "react";
+import Quill from "quill";
+import { JobCategories, JobLocations } from "../assets/assets";
+
+const AddJob = () => {
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("Paris");
+  const [category, setCategory] = useState("Cybersecurity");
+  const [level, setLevel] = useState("Entry Level");
+  const [salary, setSalary] = useState(0);
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+
+  useEffect(() => {
+    //initialisation de quill une seule fois
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
+  return (
+    
+    <form className="container p-4 flex flex-col w-full items-start gap-4">
+      
+      <div className="w-full">
+        <p className="mb-2">Job Title</p>
+        <input
+        // max-w-lg  signifie :"Limite la largeur maximale de cet élément à la taille lg" 
+          className="w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded"
+          type="text"
+          placeholder="Enter The Job Title"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+          required
+        />
+      </div>
+
+      <div className="w-full max-w-lg">
+        <p className="mb-2">Job Description</p>
+        <div className="" ref={editorRef}></div>
+      </div>
+
+      <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
+        <div className="">
+          <p className="mb-2">Job Category</p>
+          <select
+          className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+          >
+            {JobCategories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="">
+          <p className="mb-2">Job Location</p>
+          <select
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
+          >
+            {JobLocations.map((location, index) => (
+              <option key={index} value={location}>
+                {location}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="">
+          <p className="mb-2">Job Level</p>
+          <select
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+            onChange={(e) => {
+              setLevel(e.target.value);
+            }}
+          >
+            <option value="Entry Level">Entry Level</option>
+            <option value="Executive">Executive</option>
+            <option value="Director">Director</option>
+            <option value="Mid-Senior level">Mid-Senior level</option>
+            <option value="Senior level">Senior level</option>
+            <option value="Associate">Associate</option>
+            <option value="Internship">Internship</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="">
+        <p className="mb-2">Job Salary</p>
+        <input
+        className="w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]"
+          onChange={(e) => {
+            setSalary(e.target.value);
+          }}
+          type="number"
+          placeholder="2590"
+          min={0}
+        />
+      </div>
+
+      <button className="w-40 py-3 mt-4 bg-fuchsia-600 text-white rounded transition duration-300 hover:scale-105">Click To Add Job</button>
+    </form>
+  );
+};
+
+export default AddJob;
