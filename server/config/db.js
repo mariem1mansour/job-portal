56241c3cc428bd9a334c8d761537b47b84f891d6
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-07-11 19:51:00 +0200

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
- await mongoose.connect(process.env.MONGODB_URI + "/job-portal");
+    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
+//  await mongoose.connect(process.env.MONGODB_URI + "/job-portal");
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.error("Error connecting to MongoDB ❌:", error.message);
