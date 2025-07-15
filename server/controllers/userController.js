4249fe83f585397c0914e6c133fa2df970e1e23c
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-07-15 12:44:16 +0200

    Create get User Data function in the userControllers + Create jobApplication Schema

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,38 @@
-
-
+import User from "../models/User.js";
 
 //get user donnee
-export const getUserData = async (req, res) => {};
+export const getUserData = async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User Not Found ⚠️",
+      });
+    }
+
+    res.json({
+      success:true , user
+    })
+
+
+  } catch (error) {
+
+    res.json({
+      success:false,
+      message:error.message
+    })
+  }
+};
 
 //Apply for a job
-export const applyForJob = async (req, res) => {};
+export const applyForJob = async (req, res) => {
+
+  
+};
 
 //get user applied jobs
 
