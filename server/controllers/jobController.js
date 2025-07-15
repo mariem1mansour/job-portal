fb2e7f14e0ee51c24c36669f90b8a18afafe48a3
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-07-15 11:24:08 +0200

    Create Get Job By id function in the job controller , and add it to the job routes + Testing with postman

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -3,16 +3,43 @@ import Job from "../models/Job.js";
 //get all jobs
 export const getJobs = async (req, res) => {
   try {
-    const jobs = await Job.find({ visible: true })
-    .populate({path:'companyId', select:'-password'})
+    const jobs = await Job.find({ visible: true }).populate({
+      path: "companyId",
+      select: "-password",
+    });
 
-
-    res.json({success:true , jobs})
+    res.json({ success: true, jobs });
   } catch (error) {
-    res.json({success:false , message:error.message})
+    res.json({ success: false, message: error.message });
   }
 };
 
 //get a single job by id
 
-export const getJobById = async (req, res) => {};
+export const getJobById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const job = await Job.findById(id).populate({
+      path: "companyId",
+      select: "-password",
+    });
+
+    if (!job) {
+      res.json({
+        success: false,
+        message: "Job Not Found ⚠️",
+      });
+    }
+
+    res.json({
+      success:true,
+      job
+    })
+  } catch (error) {
+    res.json({
+      success:false,
+      message:error.message
+    })
+  }
+};
